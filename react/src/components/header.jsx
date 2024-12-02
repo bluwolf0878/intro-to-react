c2615b092ce538663a4089e23c6b3b0d2aed0936
andreas <andr493i@elev.techcollege.dk>
2024-12-02 12:51:20 +0100

    styling

--- react/src/components/header.jsx
+++ react/src/components/header.jsx
@@ -1,9 +1,10 @@
 const Header = () => {
     return (
       <header>
+        <h1 class clean>Minima quis perferendis unde doloremque consequuntur veniam iure dolorum, facilis, odio eveniet dolores dolore! Voluptatem possimus aliquid eius velit obcaecati aut saepe?</h1>
         <nav>
           <ul>
-            <li></li>
+            <li><link rel="stylesheet" href="here" /></li>
             <li></li>
             <li></li>
           </ul>
--- react/src/components/main.jsx
+++ react/src/components/main.jsx
@@ -1,6 +1,9 @@
 const Main = () => {
     return (
-      <main></main>
+      <main>
+        <h2>Dolores minima quo tenetur adipisci, illo rem blanditiis quibusdam quaerat laboriosam eaque dolorum magnam sit beatae ullam necessitatibus vel, doloribus atque. Dolorem.</h2>
+        <h3>Sint saepe iure praesentium nulla ipsum veniam repellendus facilis accusamus eos ut voluptatum dignissimos accusantium eveniet dolorem qui, illum odio, optio quis.</h3>
+      </main>
     );
   };
   export default Main

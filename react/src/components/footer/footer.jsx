1b717807d4659feff0abc69c2c5a4cdfe6fc6031
andreas <andr493i@elev.techcollege.dk>
2024-12-02 13:21:46 +0100

    done

--- react/src/components/footer/footer.jsx
+++ react/src/components/footer/footer.jsx
@@ -1,5 +1,7 @@
 export const Footer = () => {
     return (
-      <footer></footer>
+      <footer>
+        <h4>Excepturi provident, quo obcaecati quia, ut doloribus libero pariatur distinctio placeat, quas dolorem maxime laborum cupiditate laboriosam minus? Cum et ipsam quo.</h4>
+      </footer>
     );
   };
